4fbec3a9a0e66f5b26c7a4f84db4bad99f8a7a8b
Dave Gurnell <dave@untyped.com>
2012-04-09 14:50:30 +0100

    Publish to ivy.untyped.com

--- project/Build.scala
+++ project/Build.scala
@@ -8,15 +8,24 @@ object Build extends Build {
     id = "root",
     base = file("."),
     settings = Project.defaultSettings ++ Seq(
-      sbtPlugin := true,
       organization := "com.untyped",
       version := "0.1",
+      name := "tipi",
       scalaVersion := "2.9.1",
       scalacOptions += "-deprecation",
       scalacOptions += "-unchecked",
       libraryDependencies ++= Seq(
         scalatest % "test"
-      )
+      ),
+      publishTo := {
+        for {
+          host    <- Option(System.getenv("DEFAULT_IVY_REPO_HOST"))
+          path    <- Option(System.getenv("DEFAULT_IVY_REPO_PATH"))
+          user    <- Option(System.getenv("DEFAULT_IVY_REPO_USER"))
+          keyfile <- Option(System.getenv("DEFAULT_IVY_REPO_KEYFILE"))
+        } yield Resolver.sftp("Untyped", host, path).as(user, file(keyfile))
+      },
+      publishMavenStyle := false
     )
   )
 }
